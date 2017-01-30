3ef54b802a7305796458938167383ce3374581cb
Maryam Shahsavari <m.shahsavari65@gmail.com>
2017-01-31 02:41:12 +0330

    -edit notFoundExp class

--- src/main/java/com/yasan/exceptions/NotFoundException.java
+++ src/main/java/com/yasan/exceptions/NotFoundException.java
@@ -3,8 +3,12 @@ package com.yasan.exceptions;
 /**
  * Created by pooya on 1/13/17.
  */
-public class NotFoundException extends RuntimeException{
+public class NotFoundException extends RuntimeException {
     public NotFoundException(String s) {
         super(s);
+    }
+
+    public NotFoundException(Throwable cause) {
+        super(cause);
     }
 }
